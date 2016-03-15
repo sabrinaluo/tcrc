3c0428a5675e4812a3b3aa5b795ce3751fe06ba8
sabrinaluo <luohaojing@gmail.com>
2016-03-15 21:35:46 +0800

    add env config

--- config.js
+++ config.js
@@ -1,6 +1,11 @@
+var _ = require('lodash');
+var env = require('./env.json');
+var nodeEnv = process.env.NODE_ENV || 'dev';
+
+// put common config here; put env config into env.json
 var config = {
-  hostname: 'localhost',
-  port: 80
+  sessionSecret: 'tcrc'
 };
 
+_.assignIn(config, env[nodeEnv]);
 module.exports = config;
