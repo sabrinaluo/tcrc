0cf646864298d58ceddde6c43b2182e542e8b2f1
sabrinaluo <luohaojing@gmail.com>
2016-03-16 22:03:29 +0800

    add API: login

--- app.js
+++ app.js
@@ -2,15 +2,29 @@ var path = require('path');
 var express = require('express');
 var session = require('express-session');
 var bodyParser = require('body-parser');
-var passport = require('passport');
+var cookieParser = require('cookie-parser');
+var morgan = require('morgan');
 var config = require('./config');
 var apiRouter = require('./apiRouter');
+var auth = require('./middleware/auth');
 
 var staticDir = path.join(__dirname, 'dist');
 var app = express();
+
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(session({
+  secret: config.sessionSecret,
+  saveUninitialized: true,
+  resave: true
+}));
+app.use(morgan('dev'));
+
 app.use('/', express.static(staticDir));
-// app.use('/api/', apiRouter);
+app.use('/api/', apiRouter);
 
 app.listen(config.port, function () {
   console.log('app is listening on port: ' + config.port);
 });
+
+module.exports = app;
